442d45a93ee61c551c77e762c7c046a3a7bd638e
Harshit B <harshitbhatt972@gmail.com>
2020-10-25 00:18:13 +0530

    Weired algorithm done

--- C++/Weird Algorithm.cpp
+++ C++/Weird Algorithm.cpp
@@ -1,59 +1,26 @@
 //Weird Algorithm
 #include<bits/stdc++.h>
 using namespace std;
-void algo (int n)
+void algo (long long n)
 {
-	while(n != 1)
+	while(n!=1)
 	{
 		if(n%2 == 0)
 		{
-			n=n/2;
-			cout<<n<<"even ";
+			n = n/2;
+			cout<<n<<" ";
 		}
 		else{
-			n=(n*3)+1;
-			cout<<n<<"odd ";
+			n = (n*3)+1;
+			cout<<n<<" ";
 		}
 	}
 }
 int main()
 {
-	int n;
+	long long n;
 	cin>>n;
-	cout<<n<<"def ";
-	algo(n);
-}
-//////////////////////////////////////////////////
-//Weird Algorithm
-#include<bits/stdc++.h>
-using namespace std;
-void algo (int n)
-{
-	if(n == 1)
 	cout<<n<<" ";
-	else if(n%2 == 0)
-	{
-		if(n/2 != 1)
-		{
-			cout<<n/2<<" ";
-			algo(n/2);
-		}
-		else algo(n/2);
-	}
-	else{
-		cout<<(n*3)+1<<" ";
-		algo((n*3)+1);
-	}
-}
-int main()
-{
-	int n;
-	cin>>n;
-	if(n!=1)
-	{
-		cout<<n<<" ";
-		algo(n);
-	}
-	else
-	cout<<n;
+	algo(n);
+	cout<<" ";
 }
